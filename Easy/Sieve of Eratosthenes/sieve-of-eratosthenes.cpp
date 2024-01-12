6a6f878ac58ec4d4756d62ad19eb1076d5cd8745
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2024-01-12 23:19:13 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Sieve of Eratosthenes/sieve-of-eratosthenes.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    
+    vector<int> sieveOfEratosthenes(int N)
+    {
+        // Write Your Code here
+        vector<int> Sieve(N+1,1);
+        Sieve[0]=0;
+        Sieve[1]=0;
+        for(int i=2;i*i<=N;i++)
+        {
+            if(Sieve[i]==1)
+            {
+                for(int j=i*i;j<=N;j+=i)
+                {
+                    Sieve[j]=0;
+                }
+            }
+        }
+        vector<int> pb;
+        for(int i=2;i<=N;i++)
+        {
+            if(Sieve[i]==1)
+            {
+                pb.push_back(i);   
+            }
+        }
+        return pb;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        vector<int> primes  = ob.sieveOfEratosthenes(N);
+        for(auto prime : primes) {
+            cout<< prime <<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
