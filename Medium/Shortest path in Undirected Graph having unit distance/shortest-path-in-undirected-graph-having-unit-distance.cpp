0b6771be42c2c15c8b0b15363dcea38b3647dfc3
Mahe2112s <96754461+Mahe2112s@users.noreply.github.com>
2024-02-16 10:55:47 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Shortest path in Undirected Graph having unit distance/shortest-path-in-undirected-graph-having-unit-distance.cpp
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+void BFS(vector<int>adj[],int node,vector<int>&minpath){
+    queue<int>q;
+    q.push(node);
+    minpath[node]=0;
+    while(!q.empty())
+    {
+        int t=q.front();
+       // cout<<t<<" ";
+        q.pop();
+        for(auto it:adj[t]){
+            //cout<<it<<" ";
+            if(minpath[it]==-1){
+                q.push(it);
+                minpath[it]=minpath[t]+1;
+            }
+        }
+    //    cout<<endl;
+    }
+}
+    vector<int> shortestPath(vector<vector<int>>& edges, int N,int M, int src){
+        vector<int>minpath(N+1,-1);
+        vector<int>adj[N+1];
+        int e=edges.size();
+        for(int i=0;i<e;i++)
+        {
+            int u=edges[i][0];
+            int v=edges[i][1];
+            
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        BFS(adj,src,minpath);
+        minpath.pop_back();
+        // if(minpath[0]==-1)  minpath.erase(minpath.begin()+0);
+        // else minpath.pop_back();
+        return minpath;
+        
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m; cin >> n >> m;
+        vector<vector<int>> edges;
+
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for(int j=0; j<2; ++j){
+                int x; cin>>x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+
+        int src; cin >> src;
+
+        Solution obj;
+
+        vector<int> res = obj.shortestPath(edges, n, m, src);
+
+        for (auto x : res){
+            cout<<x<<" ";
+        }
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
