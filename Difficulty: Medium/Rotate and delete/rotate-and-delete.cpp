11a94630720823e74ba61b8b2e7d50423202e0ab
Sai Mahesh Koppisetti <96754461+Mahe2112s@users.noreply.github.com>
2024-10-02 18:50:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rotate and delete/rotate-and-delete.cpp
+++ Difficulty: Medium/Rotate and delete/rotate-and-delete.cpp
@@ -11,14 +11,17 @@ class Solution {
         // Your code here
         int n=arr.size();
         if(n==1) return arr[0];
-        int l=1;
+        int l=0;
         while(n>1){
+            l++;
             int last=arr[n-1];
-            arr.pop_back();
-            arr.insert(arr.begin(),last);
-            if(n-l<0) arr.erase(arr.begin()+0);
-            else arr.erase(arr.begin()+(n-l));
-            l+=1;
+            for(int i=n-1;i>=1;i--)
+            {
+               arr[i]=arr[i-1];
+            }
+            arr[0]=last;
+            if(l<=arr.size())arr.erase(arr.begin()+(n-l));
+            else arr.erase(arr.begin()+0);
             n--;
         }
         return arr[0];
